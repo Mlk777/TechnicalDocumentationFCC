3cf60a9404bd0850dc0cf10b3e2626160399755a
mlk777 <>
2018-11-18 15:42:29 +0100

    Changes on package.json

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 app.listen(port);
 
-console.log('server start'+ port);
+console.log("server start" + port);
